24c819e0737b3c319948a612e4bf785e4de0e667
Rain <rain@sunshowers.io>
2025-02-04 06:57:01 +0000

    [spr] changes to main this commit is based on
    Created using spr 1.3.6-beta.1
    
    [skip ci]

--- daft-derive/tests/fixtures/valid/output/generic-struct.output.rs
+++ daft-derive/tests/fixtures/valid/output/generic-struct.output.rs
@@ -1,30 +1,67 @@
-#[derive(Debug, PartialEq, Eq)]
-struct StructWithGenericsDiff<'d, 'e, 'daft, T, U>
+struct StructWithGenericsDiff<'__daft, 'd: '__daft, 'e: '__daft, T: '__daft, U: '__daft>
 where
-    T: daft::Diffable<'d>,
-    U: daft::Diffable<'e>,
+    T: daft::Diffable + 'd + ?Sized,
+    U: daft::Diffable + 'e + ?Sized,
 {
-    b: <usize as daft::Diffable<'daft>>::Diff,
-    c: <T as daft::Diffable<'d>>::Diff,
-    d: <U as daft::Diffable<'e>>::Diff,
+    b: <usize as daft::Diffable>::Diff<'__daft>,
+    c: <&'d T as daft::Diffable>::Diff<'__daft>,
+    d: <&'e U as daft::Diffable>::Diff<'__daft>,
 }
-impl<
-    'd: 'daft,
-    'e: 'daft,
-    'daft,
-    T: Eq + Debug + 'daft,
-    U: Eq + Debug + 'daft,
-> daft::Diffable<'daft> for StructWithGenerics<'d, 'e, T, U>
+impl<'__daft, 'd: '__daft, 'e: '__daft, T: '__daft, U: '__daft> ::std::fmt::Debug
+for StructWithGenericsDiff<'__daft, 'd, 'e, T, U>
 where
-    T: daft::Diffable<'d>,
-    U: daft::Diffable<'e>,
+    T: daft::Diffable + 'd + ?Sized,
+    U: daft::Diffable + 'e + ?Sized,
+    <usize as daft::Diffable>::Diff<'__daft>: ::std::fmt::Debug,
+    <&'d T as daft::Diffable>::Diff<'__daft>: ::std::fmt::Debug,
+    <&'e U as daft::Diffable>::Diff<'__daft>: ::std::fmt::Debug,
 {
-    type Diff = StructWithGenericsDiff<'d, 'e, 'daft, T, U>;
-    fn diff(&'daft self, other: &'daft Self) -> Self::Diff {
+    fn fmt(&self, f: &mut ::std::fmt::Formatter<'_>) -> ::std::fmt::Result {
+        f.debug_struct(stringify!(StructWithGenericsDiff))
+            .field(stringify!(b), &self.b)
+            .field(stringify!(c), &self.c)
+            .field(stringify!(d), &self.d)
+            .finish()
+    }
+}
+impl<'__daft, 'd: '__daft, 'e: '__daft, T: '__daft, U: '__daft> ::std::cmp::PartialEq
+for StructWithGenericsDiff<'__daft, 'd, 'e, T, U>
+where
+    T: daft::Diffable + 'd + ?Sized,
+    U: daft::Diffable + 'e + ?Sized,
+    <usize as daft::Diffable>::Diff<'__daft>: ::std::cmp::PartialEq,
+    <&'d T as daft::Diffable>::Diff<'__daft>: ::std::cmp::PartialEq,
+    <&'e U as daft::Diffable>::Diff<'__daft>: ::std::cmp::PartialEq,
+{
+    fn eq(&self, other: &Self) -> bool {
+        self.b == other.b && self.c == other.c && self.d == other.d
+    }
+}
+impl<'__daft, 'd: '__daft, 'e: '__daft, T: '__daft, U: '__daft> ::std::cmp::Eq
+for StructWithGenericsDiff<'__daft, 'd, 'e, T, U>
+where
+    T: daft::Diffable + 'd + ?Sized,
+    U: daft::Diffable + 'e + ?Sized,
+    <usize as daft::Diffable>::Diff<'__daft>: ::std::cmp::Eq,
+    <&'d T as daft::Diffable>::Diff<'__daft>: ::std::cmp::Eq,
+    <&'e U as daft::Diffable>::Diff<'__daft>: ::std::cmp::Eq,
+{}
+impl<'d, 'e, T, U> daft::Diffable for StructWithGenerics<'d, 'e, T, U>
+where
+    T: daft::Diffable + 'd + ?Sized,
+    U: daft::Diffable + 'e + ?Sized,
+{
+    type Diff<'__daft> = StructWithGenericsDiff<'__daft, 'd, 'e, T, U>
+    where
+        Self: '__daft;
+    fn diff<'__daft>(
+        &'__daft self,
+        other: &'__daft Self,
+    ) -> StructWithGenericsDiff<'__daft, 'd, 'e, T, U> {
         Self::Diff {
             b: daft::Diffable::diff(&self.b, &other.b),
-            c: daft::Diffable::diff(&*self.c, &*other.c),
-            d: daft::Diffable::diff(&*self.d, &*other.d),
+            c: daft::Diffable::diff(&self.c, &other.c),
+            d: daft::Diffable::diff(&self.d, &other.d),
         }
     }
 }
---
+++ daft-derive/tests/fixtures/valid/output/non-eq.output.rs
@@ -0,0 +1,78 @@
+struct InnerDiff<'__daft> {
+    a: <i32 as daft::Diffable>::Diff<'__daft>,
+    b: <i32 as daft::Diffable>::Diff<'__daft>,
+}
+impl<'__daft> ::std::fmt::Debug for InnerDiff<'__daft>
+where
+    <i32 as daft::Diffable>::Diff<'__daft>: ::std::fmt::Debug,
+    <i32 as daft::Diffable>::Diff<'__daft>: ::std::fmt::Debug,
+{
+    fn fmt(&self, f: &mut ::std::fmt::Formatter<'_>) -> ::std::fmt::Result {
+        f.debug_struct(stringify!(InnerDiff))
+            .field(stringify!(a), &self.a)
+            .field(stringify!(b), &self.b)
+            .finish()
+    }
+}
+impl<'__daft> ::std::cmp::PartialEq for InnerDiff<'__daft>
+where
+    <i32 as daft::Diffable>::Diff<'__daft>: ::std::cmp::PartialEq,
+    <i32 as daft::Diffable>::Diff<'__daft>: ::std::cmp::PartialEq,
+{
+    fn eq(&self, other: &Self) -> bool {
+        self.a == other.a && self.b == other.b
+    }
+}
+impl<'__daft> ::std::cmp::Eq for InnerDiff<'__daft>
+where
+    <i32 as daft::Diffable>::Diff<'__daft>: ::std::cmp::Eq,
+    <i32 as daft::Diffable>::Diff<'__daft>: ::std::cmp::Eq,
+{}
+impl daft::Diffable for Inner {
+    type Diff<'__daft> = InnerDiff<'__daft> where Self: '__daft;
+    fn diff<'__daft>(&'__daft self, other: &'__daft Self) -> InnerDiff<'__daft> {
+        Self::Diff {
+            a: daft::Diffable::diff(&self.a, &other.a),
+            b: daft::Diffable::diff(&self.b, &other.b),
+        }
+    }
+}
+struct OuterDiff<'__daft> {
+    inner: <Inner as daft::Diffable>::Diff<'__daft>,
+    c: <i32 as daft::Diffable>::Diff<'__daft>,
+}
+impl<'__daft> ::std::fmt::Debug for OuterDiff<'__daft>
+where
+    <Inner as daft::Diffable>::Diff<'__daft>: ::std::fmt::Debug,
+    <i32 as daft::Diffable>::Diff<'__daft>: ::std::fmt::Debug,
+{
+    fn fmt(&self, f: &mut ::std::fmt::Formatter<'_>) -> ::std::fmt::Result {
+        f.debug_struct(stringify!(OuterDiff))
+            .field(stringify!(inner), &self.inner)
+            .field(stringify!(c), &self.c)
+            .finish()
+    }
+}
+impl<'__daft> ::std::cmp::PartialEq for OuterDiff<'__daft>
+where
+    <Inner as daft::Diffable>::Diff<'__daft>: ::std::cmp::PartialEq,
+    <i32 as daft::Diffable>::Diff<'__daft>: ::std::cmp::PartialEq,
+{
+    fn eq(&self, other: &Self) -> bool {
+        self.inner == other.inner && self.c == other.c
+    }
+}
+impl<'__daft> ::std::cmp::Eq for OuterDiff<'__daft>
+where
+    <Inner as daft::Diffable>::Diff<'__daft>: ::std::cmp::Eq,
+    <i32 as daft::Diffable>::Diff<'__daft>: ::std::cmp::Eq,
+{}
+impl daft::Diffable for Outer {
+    type Diff<'__daft> = OuterDiff<'__daft> where Self: '__daft;
+    fn diff<'__daft>(&'__daft self, other: &'__daft Self) -> OuterDiff<'__daft> {
+        Self::Diff {
+            inner: daft::Diffable::diff(&self.inner, &other.inner),
+            c: daft::Diffable::diff(&self.c, &other.c),
+        }
+    }
+}
