dd78931f86d537524111da3a99ebcf8275a5c970
Rain <rain@sunshowers.io>
2025-02-04 08:14:24 +0000

    [spr] changes to main this commit is based on
    Created using spr 1.3.6-beta.1
    
    [skip ci]

--- daft-derive/src/lib.rs
+++ daft-derive/src/lib.rs
@@ -2,8 +2,10 @@ mod internals;
 
 use syn::parse_macro_input;
 
-#[proc_macro_derive(Diff, attributes(daft))]
-pub fn derive_diff(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+#[proc_macro_derive(Diffable, attributes(daft))]
+pub fn derive_diffable(
+    input: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
     let input = parse_macro_input!(input as syn::DeriveInput);
-    internals::derive_diff(input).into()
+    internals::derive_diffable(input).into()
 }
--- daft-derive/tests/fixtures/valid/output/basic.output.rs
+++ daft-derive/tests/fixtures/valid/output/basic.output.rs
@@ -1,14 +1,45 @@
-#[derive(Debug, PartialEq, Eq)]
-struct BasicDiff<'daft> {
-    a: <i32 as daft::Diffable<'daft>>::Diff,
-    b: <BTreeMap<Uuid, BTreeSet<usize>> as daft::Diffable<'daft>>::Diff,
+struct BasicDiff<'__daft> {
+    a: <i32 as ::daft::Diffable>::Diff<'__daft>,
+    b: <BTreeMap<Uuid, BTreeSet<usize>> as ::daft::Diffable>::Diff<'__daft>,
 }
-impl<'daft> daft::Diffable<'daft> for Basic {
-    type Diff = BasicDiff<'daft>;
-    fn diff(&'daft self, other: &'daft Self) -> Self::Diff {
+impl<'__daft> ::std::fmt::Debug for BasicDiff<'__daft>
+where
+    <i32 as ::daft::Diffable>::Diff<'__daft>: ::std::fmt::Debug,
+    <BTreeMap<
+        Uuid,
+        BTreeSet<usize>,
+    > as ::daft::Diffable>::Diff<'__daft>: ::std::fmt::Debug,
+{
+    fn fmt(&self, f: &mut ::std::fmt::Formatter<'_>) -> ::std::fmt::Result {
+        f.debug_struct(stringify!(BasicDiff))
+            .field(stringify!(a), &self.a)
+            .field(stringify!(b), &self.b)
+            .finish()
+    }
+}
+impl<'__daft> ::std::cmp::PartialEq for BasicDiff<'__daft>
+where
+    <i32 as ::daft::Diffable>::Diff<'__daft>: ::std::cmp::PartialEq,
+    <BTreeMap<
+        Uuid,
+        BTreeSet<usize>,
+    > as ::daft::Diffable>::Diff<'__daft>: ::std::cmp::PartialEq,
+{
+    fn eq(&self, other: &Self) -> bool {
+        self.a == other.a && self.b == other.b
+    }
+}
+impl<'__daft> ::std::cmp::Eq for BasicDiff<'__daft>
+where
+    <i32 as ::daft::Diffable>::Diff<'__daft>: ::std::cmp::Eq,
+    <BTreeMap<Uuid, BTreeSet<usize>> as ::daft::Diffable>::Diff<'__daft>: ::std::cmp::Eq,
+{}
+impl ::daft::Diffable for Basic {
+    type Diff<'__daft> = BasicDiff<'__daft> where Self: '__daft;
+    fn diff<'__daft>(&'__daft self, other: &'__daft Self) -> BasicDiff<'__daft> {
         Self::Diff {
-            a: daft::Diffable::diff(&self.a, &other.a),
-            b: daft::Diffable::diff(&self.b, &other.b),
+            a: ::daft::Diffable::diff(&self.a, &other.a),
+            b: ::daft::Diffable::diff(&self.b, &other.b),
         }
     }
 }
