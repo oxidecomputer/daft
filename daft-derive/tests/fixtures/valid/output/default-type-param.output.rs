fe09ae5be7c4a5d574bf191bb043ce513cf62350
Rain <rain@sunshowers.io>
2025-02-04 01:10:21 +0000

    [spr] initial version
    Created using spr 1.3.6-beta.1

---
+++ daft-derive/tests/fixtures/valid/output/default-type-param.output.rs
@@ -0,0 +1,19 @@
+#[derive(Debug, PartialEq, Eq)]
+struct StructWithDefaultTypeParamDiff<'daft, T>
+where
+    for<'x> T: Diffable<'x>,
+{
+    field: <T as daft::Diffable<'daft>>::Diff,
+}
+impl<'daft, T: Eq + Debug + 'daft> daft::Diffable<'daft>
+for StructWithDefaultTypeParam<T>
+where
+    for<'x> T: Diffable<'x>,
+{
+    type Diff = StructWithDefaultTypeParamDiff<'daft, T>;
+    fn diff(&'daft self, other: &'daft Self) -> Self::Diff {
+        Self::Diff {
+            field: daft::Diffable::diff(&self.field, &other.field),
+        }
+    }
+}
