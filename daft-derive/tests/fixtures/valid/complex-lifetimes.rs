3f9f798e92ceeeb12bd4cd6c870044883b834e54
Rain <rain@sunshowers.io>
2025-02-04 07:00:17 +0000

    [spr] changes to main this commit is based on
    Created using spr 1.3.6-beta.1
    
    [skip ci]

---
+++ daft-derive/tests/fixtures/valid/complex-lifetimes.rs
@@ -0,0 +1,20 @@
+use daft::Diff;
+use std::{cell::Cell, fmt::Debug, marker::PhantomData};
+
+#[derive(Debug, Eq, PartialEq, Diff)]
+// 'daft is named as such to test potential clashes with generated code.
+struct S<'a, 'b, 'daft: 'a, 'inv, 'contra> {
+    // Multiple lifetime params.
+    multi_ref: &'a &'b Vec<u8>,
+
+    // Lifetime param with a lifetime bound.
+    bound_ref: &'daft Vec<u8>,
+
+    // Invariant lifetime parameter.
+    inv_ref: PhantomData<Cell<&'inv ()>>,
+
+    // Contravariant lifetime parameter.
+    contra_ref: PhantomData<fn(&'contra ())>,
+}
+
+fn main() {}
